241743ce75cf0f1fa189752dfba7ff76844be604
pedzed <pedzed@outlook.com>
2014-07-13 03:49:52 +0200

    Fixed bug: variable name displaying instead of value.

--- Tres/core/View.php
+++ Tres/core/View.php
@@ -10,6 +10,13 @@ namespace Tres\core {
     
     class View {
         
+        /**
+         * The content of the view.
+         * 
+         * @var string
+         */
+        protected static $_content = '';
+        
         // Prevents instantiation
         private function __construct(){}
         private function __destruct(){}
@@ -27,15 +34,20 @@ namespace Tres\core {
             
             ob_start();
             require_once($view);
-            $content = ob_get_contents();
+            self::$_content = ob_get_contents();
             ob_end_clean();
             
             self::_processVariables($_viewData);
             
-            //echo pd(HTML::specialchars($content));
-            echo $content;
+            //echo pd(HTML::specialchars(self::$_content));
+            echo self::$_content;
         }
         
+        /**
+         * Checks if the given view is readable.
+         * 
+         * @param  string $view Path to the view.
+         */
         protected static function _checkIsReadable($view){
             try {
                 if(!is_readable($view)){
@@ -43,7 +55,7 @@ namespace Tres\core {
                 }
             } catch(ControllerException $e){
                 // TODO: Log error to file.
-                // TODO: Move to separate file
+                // TODO: Move to separate file.
                 if(Config::get('app/debug') == 1){
                     echo $e->getMessage();
                 } else if(Config::get('app/debug') == 2){
@@ -52,16 +64,22 @@ namespace Tres\core {
             }
         }
         
-        protected static function _processVariables($content, array $_viewData = array()){
+        /**
+         * Changes "{{ $var }}" to echoed PHP variables.
+         * 
+         * @param  array $_viewData The key/value pairs to process.
+         */
+        protected static function _processVariables(array $_viewData){
             // Strictly matches {{ $var }}
             $pattern = '#{{ \$(.+?) }}#';
-            return preg_replace_callback($pattern, function($_variable) use ($_viewData){
+            
+            self::$_content = preg_replace_callback($pattern, function($_variable) use ($_viewData){
                 if(isset($_viewData[$_variable[1]])){
                     return HTML::specialchars($_viewData[$_variable[1]]);
                 } else {
                     throw new ViewException('No variable "$'.$_variable[1].'" exists.');
                 }
-            }, $content);
+            }, self::$_content);
         }
         
     }
